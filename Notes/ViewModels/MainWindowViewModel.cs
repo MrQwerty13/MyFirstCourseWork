3ed4fd408706b2f76bb29e3987571e3067e4c06e
MrQwerty13 <pozur07@icloud.com>
2026-03-04 00:15:17 +0300

    Were created all very beginning part to start develop

---
+++ Notes/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Notes.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Notes/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Notes.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
